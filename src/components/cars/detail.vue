c705ac8e16c494fe62bc4337aa397a51fbce85d9
ikuosaito1989 <ikuosaito1989@gmail.com>
2024-07-24 17:56:29 +0900

    white-space見直し #57

--- src/components/cars/detail.vue
+++ src/components/cars/detail.vue
@@ -95,8 +95,8 @@ const onClickReport = () => {
     </v-carousel>
 
     <Price :is-omakase="true" class="tw-my-3" :price="car.price"></Price>
-    <div class="tw-text-xs">
-      おまかせ代行サービスは購入から納車までおまかせできるサービスです。詳しくは
+    <div class="tw-text-xs"
+      >おまかせ代行サービスは購入から納車までおまかせできるサービスです。詳しくは
       <Anchor class="tw-font-bold tw-text-[#1976d2]" to="/info/omakase-agent">こちら</Anchor>
     </div>
     <Item label="走行距離">
@@ -128,12 +128,12 @@ const onClickReport = () => {
       <div v-html="comment"></div>
     </Item>
 
-    <v-alert prominent border="top" type="warning">
-      掲載されている情報は正確でない場合や情報が更新されている可能性があります。正しい情報は掲載元ページをご確認ください
+    <v-alert prominent border="top" type="warning"
+      >掲載されている情報は正確でない場合や情報が更新されている可能性があります。正しい情報は掲載元ページをご確認ください
     </v-alert>
 
-    <v-btn class="tw-mt-5 tw-w-full" variant="elevated" @click="onGotoPage">掲載ページへ</v-btn>
-    <v-btn color="gray" variant="elevated" class="tw-mt-5 tw-w-full" @click="onClickReport"
+    <v-btn class="tw-mt-5 tw-w-full" variant="flat" @click="onGotoPage">掲載ページへ</v-btn>
+    <v-btn color="gray" variant="flat" class="tw-mt-5 tw-w-full" @click="onClickReport"
       >公開停止、または問題を報告する</v-btn
     >
     <div v-if="keywords.keywords.length > 0">
--- src/components/concept.vue
+++ src/components/concept.vue
@@ -6,12 +6,11 @@
       <v-img class="!tw-flex-[0_0_200px]" height="80" contain position="left" src="/logo.png" />
       <div class="!tw-flex-[1_1_300px]">
         <div class="tw-border-b tw-border-[#f67b01] tw-text-xl">olcar（オルカー）について</div>
-        <div class="tw-my-3 tw-text-sm">
-          olcarのコンセプトの
-          <strong>「車を身近に」</strong>するために考えていることです。<br />
-          短いのでぜひご覧ください。
-          <Anchor class="tw-mt-4 tw-block tw-font-bold tw-text-[#1976d2]" to="/info/about">
-            olcarについて
+        <div class="tw-my-3 tw-text-sm"
+          >olcarのコンセプトの
+          <strong>「車を身近に」</strong>するために考えていることです。短いのでぜひご覧ください。
+          <Anchor class="tw-mt-4 tw-block tw-font-bold tw-text-[#1976d2]" to="/info/about"
+            >olcarについて
           </Anchor>
         </div>
       </div>
