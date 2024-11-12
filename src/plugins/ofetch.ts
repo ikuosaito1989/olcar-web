b3fb621773fff146a54da543bac790b940122713
ikuosaito1989 <ikuosaito1989@gmail.com>
2024-11-12 22:02:24 +0900

    ローディング不安定なので消す

--- src/plugins/ofetch.ts
+++ src/plugins/ofetch.ts
@@ -4,21 +4,12 @@ import { report } from './error-handler'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default defineNuxtPlugin((_nuxtApp) => {
-  const loader = useLoader()
   globalThis.$fetch = ofetch.create({
-    async onRequest() {
-      loader.value = true
-    },
-    async onResponse() {
-      loader.value = false
-    },
     async onRequestError({ error }) {
       await report(error!)
-      loader.value = false
     },
     async onResponseError({ error }) {
       await report(error!)
-      loader.value = false
     },
   })
 })
