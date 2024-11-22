c94564130cae592b77ed5b55496e910f141a14b6
ikuosaito1989 <ikuosaito1989@gmail.com>
2024-11-22 10:14:43 +0900

    エラーハンドリングを追加

--- src/plugins/error-handler.ts
+++ src/plugins/error-handler.ts
@@ -7,6 +7,21 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook('vue:error', async (error) => {
     await report(error as Error)
   })
+
+  nuxtApp.vueApp.config.warnHandler = async (msg, _, trace) => {
+    const error: Error = {
+      name: '',
+      message: msg,
+      stack: trace,
+    }
+    await report(error as Error)
+  }
+
+  if (import.meta.client) {
+    window.addEventListener('error', async (event) => {
+      await report(event.error as Error)
+    })
+  }
 })
 
 /**
@@ -15,15 +30,25 @@ export default defineNuxtPlugin((nuxtApp) => {
  * @param error
  */
 export const report = async (error: Error) => {
-  if ((error as NuxtError)?.statusCode === 404) {
-    return
-  }
   const runtimeConfig = useRuntimeConfig()
-  await $fetch(runtimeConfig.slackHookUrl, {
-    method: 'POST',
-    body: {
-      text: `${error.message}
-        ${error.stack}`,
-    },
-  })
+  try {
+    if ((error as NuxtError)?.statusCode === 404) {
+      return
+    }
+
+    await $fetch(runtimeConfig.slackHookUrl, {
+      method: 'POST',
+      body: {
+        text: `${error.message}
+          ${error.stack}`,
+      },
+    })
+  } catch {
+    await $fetch(runtimeConfig.slackHookUrl, {
+      method: 'POST',
+      body: {
+        text: 'エラーハンドリングでエラーになった',
+      },
+    })
+  }
 }
