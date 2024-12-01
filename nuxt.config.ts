051ad8d0cfc2a2697ff756c86c29344135c6a7f9
ikuosaito1989 <ikuosaito1989@gmail.com>
2024-12-01 10:34:31 +0900

    fix compressPublicAssets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,6 +34,10 @@ export default defineNuxtConfig({
     transpile: ['vuetify'],
   },
 
+  nitro: {
+    compressPublicAssets: true,
+  },
+
   modules: [
     'vue-recaptcha/nuxt',
     '@nuxtjs/tailwindcss',
