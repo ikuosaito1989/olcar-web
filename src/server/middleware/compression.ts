33cc88732ca971d5df3a623a8b8b695ed19f8f82
ikuosaito1989 <ikuosaito1989@gmail.com>
2024-12-06 22:16:01 +0900

    text/htmlのみ圧縮適用するよう修正

--- src/server/middleware/compression.ts
+++ src/server/middleware/compression.ts
@@ -5,8 +5,8 @@ export default defineEventHandler((event) => {
   const res = event.node.res
   const req = event.node.req
 
-  // @note ルートのみ圧縮適用する
-  if (req.url === '/') {
+  // @note text/htmlのみ圧縮適用する
+  if (req.headers['accept']?.includes('text/html')) {
     res.setHeader('Cache-Control', 'public, max-age=900')
     compression()(req, res, () => {})
   }
