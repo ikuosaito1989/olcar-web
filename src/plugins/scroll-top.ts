b1ebeb8797eb0e12acad42dd283fc5ec1e347c07
ikuosaito1989 <ikuosaito1989@gmail.com>
2024-12-28 20:18:23 +0900

    スクロールを戻すのをやめる

--- src/plugins/scroll-top.ts
+++
@@ -1,6 +0,0 @@
-// ref. https://github.com/nuxt/nuxt/discussions/16223#discussioncomment-1933574
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook('page:finish', () => {
-    window.scrollTo(0, 0)
-  })
-})
