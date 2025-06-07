d503bae140d58c7b258c11ad04f2c9f7ca1c4d1c
ikuosaito1989 <ikuosaito1989@gmail.com>
2025-06-07 23:01:24 +0900

    メンテナンス終了

--- src/middleware/maintenance.global.ts
+++ src/middleware/maintenance.global.ts
@@ -2,7 +2,7 @@
  * メンテナンス画面へ遷移する
  */
 export default defineNuxtRouteMiddleware((to) => {
-  const maintenanceMode = true
+  const maintenanceMode = false
 
   if (!maintenanceMode && to.path === '/maintenance') {
     return abortNavigation(createError({ statusCode: 404, statusMessage: 'Not Found' }))
