0cae6bbb76722872e4a9be83e4bf6798d109deb6
ikuosaito1989 <ikuosaito1989@gmail.com>
2026-03-04 00:35:10 +0900

    format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
   app: {
     keepalive: false,
     head: {
